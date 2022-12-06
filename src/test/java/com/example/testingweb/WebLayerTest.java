6cc8774a81335e1cdd0a43670bb70c2709659bdd
Keunoh <rmsdhcjswo1@gmail.com>
2022-12-06 23:20:33 +0900

    specific @WebMvcTest

--- src/test/java/com/example/testingweb/WebLayerTest.java
+++ src/test/java/com/example/testingweb/WebLayerTest.java
@@ -3,6 +3,7 @@ package com.example.testingweb;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.containsString;
@@ -11,7 +12,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest
+@WebMvcTest(HomeController.class)
 public class WebLayerTest {
 
     @Autowired
